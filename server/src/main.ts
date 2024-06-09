b91961b0ea8feb3740c67d0c9eb9c5d5ea9fb08b
João Paulo Dias <joaopdiasventura@gmail.com>
2024-06-09 00:34:11 -0300

    fix: liberando a politica cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+		origin: process.env.FRONTEND,
+		methods: ["GET", "DELETE", "POST", "PUT"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	};
+	app.enableCors(corsOptions);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
